68b9b58d4e24965d1c7da7cd67e10e286d80e1e1
Maxim Molenaars <maximmolenaars@gmail.com>
2018-07-12 17:18:11 +0200

    Added rocket.cs

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcesInput();	
+	}
+
+    private void ProcesInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+
+    }
+}
