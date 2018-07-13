f438841778689525438d37f2a8acf954f79ee1af
Maxim Molenaars <maximmolenaars@gmail.com>
2018-07-13 15:36:01 +0200

    Added audio and rocket control

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -6,33 +6,46 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
     
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcesInput();	
+        ProcesInput();
 	}
 
     private void ProcesInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) 
+            {
+                audioSource.Play();
+            }
+
+            else 
+            {
+                audioSource.Stop();
+            }
+
         }
 
-        if (Input.GetKey(KeyCode.A))
+if (Input.GetKey(KeyCode.A))
         {
             print("Rotating left");
+            transform.Rotate(Vector3.forward); //forward is z axis
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
             print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
 
     }
